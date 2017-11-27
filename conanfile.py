092313eb7d107cfb3b8a7655567dc832c4506a36
Roey Darwish Dror <roey.ghost@gmail.com>
2017-11-27 21:02:59 +0200

    msgpack

--- conanfile.py
+++ conanfile.py
@@ -6,26 +6,21 @@ import os
 
 
 class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
+    name = "msgpack"
+    version = "2.1.5"
     url = "https://github.com/bincrafters/conan-libname"
-    description = "Keep it short"
-    license = "https://github.com/someauthor/somelib/blob/master/LICENSES"
-    requires = "OpenSSL/1.0.2l@conan/stable", \
-        "zlib/1.2.11@conan/stable", \
-        "websocketpp/0.7.0@%s/%s" % (self.user, self.channel)
+    description = "MessagePack implementation for C and C++"
+    license = "https://raw.githubusercontent.com/msgpack/msgpack-c/master/LICENSE_1_0.txt"
 
     def source(self):
-        source_url = "https://github.com/Microsoft/cpprestsdk"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
-        extracted_dir = self.name + "-" + self.version
+        source_url = "https://github.com/msgpack/msgpack-c"
+        tools.get("{0}/archive/cpp-{1}.tar.gz".format(source_url, self.version))
+        extracted_dir = self.name + "-c-cpp-" + self.version
         os.rename(extracted_dir, "sources")
-        #Rename to "sources" is a convention to simplify later steps
 
     def package(self):
-        with tools.chdir("sources"):
-            self.copy(pattern="LICENSE")
-            self.copy(pattern="*", dst="include", src="include")
+        self.copy(pattern="LICENSE_1_0.txt", src="sources")
+        self.copy(pattern="*", dst="include", src="sources/include")
 
     def package_id(self):
         self.info.header_only()
--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,6 +1,36 @@
-// #include <cpprest/json.h>
+#include <msgpack.hpp>
+#include <string>
+#include <iostream>
+#include <sstream>
 
 int main()
 {
-    // const auto parsed_value = web::json::value::parse(U("-22"));
+    msgpack::type::tuple<int, bool, std::string> src(1, true, "example");
+
+    // serialize the object into the buffer.
+    // any classes that implements write(const char*,size_t) can be a buffer.
+    std::stringstream buffer;
+    msgpack::pack(buffer, src);
+
+    // send the buffer ...
+    buffer.seekg(0);
+
+    // deserialize the buffer into msgpack::object instance.
+    std::string str(buffer.str());
+
+    msgpack::object_handle oh =
+        msgpack::unpack(str.data(), str.size());
+
+    // deserialized object is valid during the msgpack::object_handle instance is alive.
+    msgpack::object deserialized = oh.get();
+
+    // msgpack::object supports ostream.
+    std::cout << deserialized << std::endl;
+
+    // convert msgpack::object instance into the original type.
+    // if the type is mismatched, it throws msgpack::type_error exception.
+    msgpack::type::tuple<int, bool, std::string> dst;
+    deserialized.convert(dst);
+
+    return 0;
 }
